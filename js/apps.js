6f6121d64e352bd2a7842e5d74ef499c17830bd8
Agustinmp97 <pichagustin@gmail.com>
2023-06-20 01:03:11 -0300

    mail-form-indexfix-darkmodefix

--- js/apps.js
+++ js/apps.js
@@ -9,3 +9,32 @@ toggle.addEventListener("change", (event) => {
     label.innerHTML = '<i class="fa-solid fa-moon"></i>';
   }
 });
+
+
+function validar(){
+  var nombre, correo, asunto;
+
+  nombre = document.getElementById("nombre").value;
+  correo = document.getElementById("correo").value;
+  asunto = document.getElementById("asunto").value;
+  
+  expresion=/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+
+  if(nombre==="" || correo==="" || asunto===""){
+    alert ("Todos los campos son obligatorios");
+    return false;
+  }else if(nombre.length>30){
+    alert ("Nombre extenso");
+    return false;
+  }else if(correo.length>100){
+    alert ("Correo extenso");
+    return false;
+  }else if(asunto.length>250){
+    alert ("Asunto extenso");
+    return false;
+  }else if(!expresion.test(correo)){
+    alert ("Email invalido")
+    return false;
+  }
+
+}
